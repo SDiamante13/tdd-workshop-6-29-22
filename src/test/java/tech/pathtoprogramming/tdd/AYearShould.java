322dcd22c9fb3e548f5897917f092be92eed54cd
Steven Diamante <stevendiamante13@gmail.com>
2022-07-27 12:37:29 -0400

    Leap Year Kata start: AssertJ failing test.

---
+++ src/test/java/tech/pathtoprogramming/tdd/AYearShould.java
@@ -0,0 +1,12 @@
+package tech.pathtoprogramming.tdd;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AYearShould {
+
+    @Test
+    void failingTest() {
+        Assertions.assertThat(true).isEqualTo(false);
+    }
+}
