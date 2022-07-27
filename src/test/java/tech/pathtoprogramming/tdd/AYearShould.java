dbba7e7ec38b40c29736b597bcc500529201071b
Steven Diamante <stevendiamante13@gmail.com>
2022-07-27 13:09:35 -0400

    Handles all cases. A leap year is divisible by 4, but is not otherwise divisible by 100 unless it is also divisible by 400.

--- src/test/java/tech/pathtoprogramming/tdd/AYearShould.java
+++ src/test/java/tech/pathtoprogramming/tdd/AYearShould.java
@@ -25,7 +25,7 @@ class AYearShould {
 class Year {
 
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             return true;
         }
         return false;
